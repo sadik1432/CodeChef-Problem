79377936bf1649fac27294ebc2ba79c1f3791872
Abdus Sadik Molla <abdussadikmolla@gmail.com>
2022-06-13 10:54:03 +0530

    add HS08TEST

---
+++ ATM_HS08TEST.C
@@ -0,0 +1,19 @@
+// PROBLEM LINK: https://www.codechef.com/submit-v2/HS08TEST 
+
+#include<stdio.h>
+int main()
+{
+    int x;
+    float y;
+    scanf("%d %f",&x,&y);
+    if(x>y){
+        printf("%.2f",y);
+    }
+    else if(x%5==0 && (x+0.5)<=y){
+        printf("%.2f",(y-(0.50)-x));
+    }
+    else{
+        printf("%.2f",y);
+    }
+    return 0;
+}//
