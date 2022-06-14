4535911ce6f27ad2ad26461ede519c4834254a5b
Abdus Sadik Molla <abdussadikmolla@gmail.com>
2022-06-14 22:50:05 +0530

    Add MY1STCONT Problem

---
+++ MY1STCONTEST.c
@@ -0,0 +1,12 @@
+// Problem link: https://www.codechef.com/submit-v2/MY1STCONTEST
+
+
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    int n,a,b;
+    scanf("%d %d %d",&n,&a,&b);
+    printf("%d %d",n-a,n-a-b);
+    return 0;
+}
