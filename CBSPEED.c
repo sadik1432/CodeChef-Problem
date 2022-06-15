c2c7fb132e0b8c426da2059f7dfeebee13789328
Abdus Sadik Molla <abdussadikmolla@gmail.com>
2022-06-15 19:12:08 +0530

    Add CBSPEED Problem

---
+++ CBSPEED.c
@@ -0,0 +1,21 @@
+// Problem link: https://www.codechef.com/submit-v2/CBSPEED
+
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    
+        int a, b;
+        scanf("%d %d", &a, &b);
+        if (a < b)
+        {
+            printf("YES");
+        }
+        else
+        {
+            printf("NO");
+        }
+    
+    return 0;
+}
